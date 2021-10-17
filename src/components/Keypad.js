88d9e81b0af2901517ea95ba1236914812292e9d
Arundhati <arundhatiraicar12@gmail.com>
2021-10-18 01:10:22 +0530

    Completed Assignment

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,10 @@
 // Code Keypad Component Here
+function Keypad(){
+    function consoling(){
+        console.log('Entering password...')
+    }
+    return(
+        <input type="password" onChange={consoling}/>
+    )
+}
+export default Keypad;
